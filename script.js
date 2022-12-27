1d1d391a5ee39014eed77957c7a63d98930ff69c
vpChar <vpchar@gmail.com>
2022-12-27 19:55:49 +0200

    3rd attempt, ALMOST works but draws over my moves ;-)

--- script.js
+++ script.js
@@ -1,147 +1,167 @@
-const squares = document.querySelectorAll(".square");
-const resetButton = document.querySelector("#reset");
-let gameOver = false;
+<script>
+  // Keep track of the game state
+  const squares = document.querySelectorAll('.square');
+  let currentPlayer = 'X';
+  let gameOver = false;
+  let gameState = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ];
 
-function resetGame() {
-  gameOver = false;
-  squares.forEach((square) => {
-    square.textContent = "";
+  // Add an event listener to each square to handle clicks
+  squares.forEach(square => {
+    square.addEventListener('click', handleClick);
   });
-}
 
-function makeMove(square) {
-  if (gameOver || square.textContent !== "") {
-    return;
-  }
-  square.textContent = "X";
-  checkForWin();
-  if (gameOver) {
-    return;
-  }
-  computerMove();
-  checkForWin();
-}
-
-function computerMove() {
-  let move = alphaBeta(squares, -Infinity, Infinity, "O").index;
-  squares[move].textContent = "O";
-}
-
-function checkForWin() {
-  // check rows
-  for (let i = 0; i < 9; i += 3) {
-    if (
-      squares[i].textContent === squares[i + 1].textContent &&
-      squares[i].textContent === squares[i + 2].textContent &&
-      squares[i].textContent !== ""
-    ) {
-      gameOver = true;
+  function handleClick(event) {
+    // Ignore clicks if the game is over or if the square is already filled
+    if (gameOver || event.target.textContent !== '') {
       return;
     }
-  }
-  // check columns
-  for (let i = 0; i < 3; i++) {
-    if (
-      squares[i].textContent === squares[i + 3].textContent &&
-      squares[i].textContent === squares[i + 6].textContent &&
-      squares[i].textContent !== ""
-    ) {
+
+    // Place the current player's symbol in the square
+    event.target.textContent = currentPlayer;
+
+    // Update the game state
+    const row = Math.floor(event.target.id / 3);
+    const col = event.target.id % 3;
+    gameState[row][col] = currentPlayer;
+
+    // Check for a win or a draw
+    const result = checkResult(gameState, currentPlayer);
+    if (result === 'X' || result === 'O') {
+      // Game over - show a message and disable clicks on the squares
       gameOver = true;
-      return;
-    }
-  }
-  // check diagonals
-  if (
-    squares[0].textContent === squares[4].textContent &&
-    squares[0].textContent === squares[8].textContent &&
-    squares[0].textContent !== ""
-  ) {
-    gameOver = true;
-    return;
-  }
-  if (
-    squares[2].textContent === squares[4].textContent &&
-    squares[2].textContent === squares[6].textContent &&
-    squares[2].textContent !== ""
-  ) {
-    gameOver = true;
-    return;
-  }
-  // check for draw
-  let emptySquares = false;
-  squares.forEach((square) => {
-    if (square.textContent === "") {
-      emptySquares = true;
-    }
-  });
-  if (!emptySquares) {
-    gameOver = true;
-  }
-}
-
-function alphaBeta(newSquares, alpha, beta, player) {
-  // check for terminal state (win or draw)
-
-  let winner = checkForWin(newSquares);
-  if (winner !== null && winner !== undefined) {
-    if (winner === "X") {
-      return { score: -10 };
-    } else if (winner === "O") {
-      return { score: 10 };
+      alert(`Player ${result} wins!`);
+      squares.forEach(square => {
+        square.removeEventListener('click', handleClick);
+      });
+    } else if (result === 'draw') {
+      // Game over - show a message and disable clicks on the squares
+      gameOver = true;
+      alert(`It's a draw!`);
+      squares.forEach(square => {
+        square.removeEventListener('click', handleClick);
+      });
     } else {
-      return { score: 0 };
+      // Switch players
+      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+
+      // If it's the computer's turn, let the computer make a move
+      if (currentPlayer === 'O') {
+        makeComputerMove();
+      }
     }
   }
-  // console.log({ newSquares, alpha, beta, player });
-  // generate list of available moves
-  let moves = [];
-  newSquares.forEach((square, index) => {
-    if (square.textContent === "") {
-      let move = { index: index };
-      if (player === "O") {
-        let result = alphaBeta(newSquares, alpha, beta, "X");
-        move.score = result.score;
-        beta = Math.min(beta, result.score);
-      } else {
-        let result = alphaBeta(newSquares, alpha, beta, "O");
-        move.score = result.score;
-        alpha = Math.max(alpha, result.score);
-      }
-      newSquares[index].textContent = "";
-      moves.push(move);
 
-      if (beta <= alpha) {
-        return;
+  function checkResult(gameState, player) {
+    // Check for a win by checking all rows, columns, and diagonals
+    if (gameState[0][0] === player && gameState[0][1] === player && gameState[0][2] === player ||
+        gameState[1][0] === player && gameState[1][1] === player && gameState[1][2] === player ||
+        gameState[2][0] === player && gameState[2][1] === player && gameState[2][2] === player ||
+        gameState[0][0] === player && gameState[1][0] === player && gameState[2][0] === player ||
+        gameState[0][1] === player && gameState[1][1] === player && gameState[2][1] === player ||
+        gameState[0][2] === player && gameState[1][2] === player && gameState[2][2] === player ||
+        gameState[0][0] === player && gameState[1][1] === player && gameState[2][2] === player ||
+        gameState[0][2] === player && gameState[1][1] === player && gameState[2][0] === player) {
+      return player;
+    }
+
+    // Check for a draw by checking if all squares are filled
+    for (let i = 0; i < gameState.length; i++) {
+      for (let j = 0; j < gameState[i].length; j++) {
+        if (gameState[i][j] === '') {
+          return '';
+        }
       }
     }
-  });
-  // choose best move
-  let bestMove;
-  if (player === "O") {
+
+    return 'draw';
+  }
+
+  function makeComputerMove() {
+    // Use the minimax algorithm to find the best move for the computer
     let bestScore = -Infinity;
-    moves.forEach((move) => {
-      if (move.score > bestScore) {
-        bestScore = move.score;
-        bestMove = move;
+    let bestRow = null;
+    let bestCol = null;
+    for (let row = 0; row < gameState.length; row++) {
+      for (let col = 0; col < gameState[row].length; col++) {
+        if (gameState[row][col] === '') {
+          gameState[row][col] = 'O';
+          const score = minimax(gameState, 0, false);
+          gameState[row][col] = '';
+          if (score > bestScore) {
+            bestScore = score;
+            bestRow = row;
+            bestCol = col;
+          }
+        }
       }
-    });
-  } else {
-    let bestScore = Infinity;
-    moves.forEach((move) => {
-      if (move.score < bestScore) {
-        bestScore = move.score;
-        bestMove = move;
-        console.log({ move });
-      }
-    });
-  }
-  // return best move
-  return bestMove;
-}
+    }
 
-squares.forEach((square) => {
-  square.addEventListener("click", () => {
-    makeMove(square);
-  });
-});
-resetButton.addEventListener("click", resetGame);
+    // Make the best move
+    gameState[bestRow][bestCol] = 'O';
+    squares[bestRow * 3 + bestCol].textContent = 'O';
+    currentPlayer = 'X';
+
+    // Check for a win or a draw
+    const result = checkResult(gameState, 'O');
+    if (result === 'O') {
+      // Game over - show a message and disable clicks on the squares
+      gameOver = true;
+      alert(`Player O wins!`);
+      squares.forEach(square => {
+        square.removeEventListener('click', handleClick);
+      });
+    } else if (result === 'draw') {
+      // Game over - show a message and disable clicks on the squares
+      gameOver = true;
+      alert(`It's a draw!`);
+      squares.forEach(square => {
+        square.removeEventListener('click', handleClick);
+      });
+    }
+  }
+
+  function minimax(gameState, depth, isMaximizing) {
+    const result = checkResult(gameState, isMaximizing ? 'O' : 'X');
+    if (result === 'O') {
+      return 10 - depth;
+    } else if (result === 'X') {
+      return depth - 10;
+    } else if (result === 'draw') {
+      return 0;
+    }
+
+    if (isMaximizing) {
+      let bestScore = -Infinity;
+      for (let row = 0; row < gameState.length; row++) {
+        for (let col = 0; col < gameState[row].length; col++) {
+          if (gameState[row][col] === '') {
+            gameState[row][col] = 'O';
+            const score = minimax(gameState, depth + 1, false);
+            gameState[row][col] = '';
+            bestScore = Math.max(bestScore, score);
+          }
+        }
+      }
+      return bestScore;
+    } else {
+      let bestScore = Infinity;
+      for (let row = 0; row < gameState.length; row++) {
+        for (let col = 0; col < gameState[row].length; col++) {
+          if (gameState[row][col] === '') {
+            gameState[row][col] = 'X';
+            const score = minimax(gameState, depth + 1, true);
+            gameState[row][col] = '';
+            bestScore = Math.min(bestScore, score);
+          }
+        }
+      }
+      return bestScore;
+    }
+  }
+</script>
+
+
